7c38302a9daf126723485407f2b759cb7b67b03a
karl-dp-eni <karl.dupuy2025@campus-eni.fr>
2026-04-27 16:08:53 +0200

    Added base html

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MainController extends AbstractController
+{
+    #[Route('', name: 'main_home')]
+    public function home(): Response
+    {
+        $username = 'Karl';
+
+        return $this->render('main/home.html.twig', [
+            'username' => $username
+        ]);
+    }
+
+    #[Route('/test', name: 'main_test')]
+    public function test(): Response
+    {
+        return $this->render('main/test.html.twig');
+    }
+}
