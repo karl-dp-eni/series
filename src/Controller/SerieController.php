f87b1e3f847704c5892b6c617d016c01c6fe571c
karl-dp-eni <karl.dupuy2025@campus-eni.fr>
2026-04-28 16:25:09 +0200

    Added fixtures and faker to generate entities and data

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,22 +13,43 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SerieController extends AbstractController
 {
     #[Route('', name: 'list')]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        // TODO : renvoyer la liste des séries
-        return $this->render('serie/list.html.twig');
+//        $series = $serieRepository->findAll();
+//        $series = $serieRepository->findBy(['status' => 'ended'], ['name' => 'ASC']);
+        $series = $serieRepository->findBy([], ['popularity' => 'DESC']);
+
+        return $this->render('serie/list.html.twig', [
+            "series" => $series
+        ]);
     }
 
     #[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function detail(int $id): Response
+    public function detail(int $id, SerieRepository $serieRepository): Response
     {
+        $serie = $serieRepository->find($id);
         // TODO : renvoyer une série
-        return $this->render('serie/detail.html.twig');
+        return $this->render('serie/detail.html.twig', [
+            'serie' => $serie
+        ]);
     }
 
     #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
-    public function create(): Response
+    public function create(EntityManagerInterface $entityManager): Response
     {
+
+        $serie = new Serie();
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        $serie->setName("Buffy contre les vampires");
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
         // TODO : créer une nouvelle série avec un formulaire
         return $this->render('serie/create.html.twig');
     }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 50; $i++) {
+            $serie = new Serie();
+            $serie
+                ->setBackdrop('backdrop.png')
+                ->setDateCreated(new \DateTime())
+                ->setFirstAirDate($faker->dateTimeBetween('-3 years'))
+                ->setName($faker->jobTitle())
+                ->setGenres($faker->randomElement(['Fantastique', 'Drama', 'SF']))
+                ->setLastAirDate($faker->dateTimeBetween($serie->getFirstAirDate()))
+                ->setPopularity($faker->numberBetween(0, 9999))
+                ->setPoster('poster.png')
+                ->setStatus($faker->randomElement(['canceled', 'returning', 'ended']))
+                ->setTmdbId($faker->randomNumber(6))
+                ->setVote($faker->numberBetween(0, 10));
+
+            $manager->persist($serie);
+        }
+
+        $manager->flush();
+    }
+}
