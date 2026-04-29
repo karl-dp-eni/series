cba03a8104da008ee2b552a57e3043c14d05b6f6
karl-dp-eni <karl.dupuy2025@campus-eni.fr>
2026-04-29 14:31:25 +0200

    Added pagination

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -12,16 +12,31 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/serie', name: 'serie_')]
 final class SerieController extends AbstractController
 {
-    #[Route('', name: 'list')]
-    public function list(SerieRepository $serieRepository): Response
+    #[Route('/list/{page}', name: 'list', requirements: ['page' => '\d+'])]
+    public function list(SerieRepository $serieRepository, int $page = 1): Response
     {
 //        $series = $serieRepository->findAll();
 //        $series = $serieRepository->findBy(['status' => 'ended'], ['name' => 'ASC']);
 //        $series = $serieRepository->findBy([], ['popularity' => 'DESC']);
-        $series = $serieRepository->findBestSeries();
+//        $series = $serieRepository->findBestSeries();
+
+        $nbSeries = $serieRepository->count();
+        $maxPage = ceil($nbSeries / 50);
+
+        if ($page < 1) {
+//            $page = 1;
+            return $this->redirectToRoute('serie_list');
+        } elseif ($page > $maxPage) {
+//            $page = $maxPage;
+            return $this->redirectToRoute('serie_list', ['page' => $maxPage]);
+        }
+
+        $series = $serieRepository->findBestSeriesWithPagination($page);
 
         return $this->render('serie/list.html.twig', [
-            "series" => $series
+            "series" => $series,
+            'currentPage' => $page,
+            'maxPage' => $maxPage
         ]);
     }
 
--- src/Repository/SerieRepository.php
+++ src/Repository/SerieRepository.php
@@ -41,4 +41,15 @@ class SerieRepository extends ServiceEntityRepository
 
         return $query->getResult();
     }
+
+    public function findBestSeriesWithPagination(int $page) : array {
+
+//        $qb = $this->createQueryBuilder('s');
+//        $qb->addOrderBy('s.popularity', 'DESC');
+        $limit = 50;
+        $offset = ($page -1) * $limit;
+
+        return $this->findBy([], ['popularity' => 'DESC'],$limit, $offset);
+
+    }
 }
